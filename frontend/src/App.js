0b38a36d7c9d40da338c5dff37b8eb73a30c8107
Tiago Dias <tigo@webdeleve.net>
2020-03-27 23:47:09 -0300

    useState example with counter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
+
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+
+  function Increment() {
+    setCounter(counter + 1);
+
+  }
+
+
   return (
-    <h1>Hello World</h1>
+    
+    <div>
+      <Header>
+        Contador: {counter}
+      </Header>
+      <button onClick={Increment} >Incrementar
+      </button >
+    </div>
+
     );
 }
 
---
+++ frontend/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header({children}) {
+
+  return (
+
+    <header>
+      <h1>
+        {children}
+      </h1>
+    </header>
+
+  );
+
+}
+
+export default Header;
